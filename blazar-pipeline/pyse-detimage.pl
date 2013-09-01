f34ed533d23aff87313483dd7663c713fb3e386b
Martin Hardcastle <hardcastle@lofar.eu>
2013-09-01 11:21:34 +0000

    pyse-detimage localization

--- blazar-pipeline/pyse-detimage.pl
+++ blazar-pipeline/pyse-detimage.pl
@@ -2,12 +2,14 @@
 
 # Testing the use of the detimage variant of pyse
 
-#$PYSE_PATH="/home/swinbank/pyse-detimage";
-
-#$ENV{'LD_LIBRARY_PATH'}=$PYSE_PATH."/lib:".$ENV{'LD_LIBRARY_PATH'};
-#$ENV{'PYTHONPATH'}=$PYSE_PATH."/lib/python2.6/site-packages:".$ENV{'PYTHONPATH'};
-
-$pyse="pyse.py";
+if (-d"/home/swinbank") {
+    $PYSE_PATH="/home/swinbank/pyse-detimage";
+    $ENV{'LD_LIBRARY_PATH'}=$PYSE_PATH."/lib:".$ENV{'LD_LIBRARY_PATH'};
+    $ENV{'PYTHONPATH'}=$PYSE_PATH."/lib/python2.6/site-packages:".$ENV{'PYTHONPATH'};
+    $pyse=$PYSE_PATH."/bin/pyse.py";
+} else {
+    $pyse="pyse.py";
+}
 
 foreach(@ARGV) {
     $file=$_;
